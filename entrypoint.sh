c689a8e226938f1c799ace547b27d98b68bba8b2
Luis Ribeiro <luis.mribeiro@sapo.pt>
2022-01-20 22:31:16 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,9 @@ then
 	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chown -R $4:$4 $3"
 	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 775 -R $3/web"
 	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 777 -R $3/runtime"
+	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 777 -R $3/web/app"
 	ssh -i /root/.ssh/id_rsa -t $1@$2 "sudo chmod 777 -R $3/web/assets"
+	ssh -i /root/.ssh/id_rsa -t $1@$2 "php yii migrate --interactive=0"
 
 	echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
 	exit 0
