51ea39f96e4d6038b855be093ffc2f46a55ba683
Luis Ribeiro <luis.mribeiro@sapo.pt>
2025-03-05 17:17:05 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,22 +33,12 @@ then
 	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
 
 	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chown -R $4:$4 $3"
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 775 -R $3/web"
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/runtime",
- 	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/export",
- 	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/import"
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/messages/*"
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/web/app"
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/web/assets"
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/web/images/catalog"
+	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 775 -R $3/protected/runtime"
+	ssh -i /root/.ssh/id_rsa -tt $1@$2 "sudo chmod 777 -R $3/public/assets"
 	
 	echo $'\n' "------ RUN MIGRATIONS -------------------------" $'\n'
 	
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "php $3/yii migrate --interactive=0"
-
- 	echo $'\n' "------ UPDATE TRANSLATIONS -------------------------" $'\n'
-	
-	ssh -i /root/.ssh/id_rsa -tt $1@$2 "php $3/yii message/extract $3/config/languages.php --messagePath=$3/messages"
+	ssh -i /root/.ssh/id_rsa -tt $1@$2 "php $3/yiic migrate up --interactive=0"
 
 	echo $'\n' "------ CONGRATS! DEPLOY SUCCESSFUL!!! ---------" $'\n'
 	exit 0
